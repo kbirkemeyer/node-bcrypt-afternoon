4d548fac4e88db8f1243acebc0210f0710456f27
Katie <adlerk8@gmail.com>
2020-11-10 14:16:20 -0700

    in progress step 2 part 1

---
+++ server/index.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const express = require('express');
+const session = require('express-session');
+const massive = require('massive');
+const authCtrl = require('./controllers/authController');
+const {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env;
+
+const app = express();
+app.use(express.json());
+
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: {rejectUnauthorized: false}
+}).then((db) => {
+    app.set('db', db);
+});
+
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: SESSION_SECRET,
+    cookie: {maxAge: 1000 * 60 * 60 * 24}
+}));
+
+app.post('/auth/register', authCtrl.register)
+
+app.listen(SERVER_PORT, () => console.log(`Server running on port ${SERVER_PORT}, m'lady!`));
