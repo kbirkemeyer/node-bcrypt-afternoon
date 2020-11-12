c12090dc39028994e6625467fee151e5426c19c6
Katie <adlerk8@gmail.com>
2020-11-11 21:04:01 -0700

    in progress step 8 part 1

--- src/Components/AddTreasure.js
+++ src/Components/AddTreasure.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class AddTreasure extends Component {
   constructor() {
@@ -14,6 +15,19 @@ export default class AddTreasure extends Component {
 
   addTreasure() {
     // post to /api/treasure/user here
+    const {treasureURL} = this.state;
+    const {addMyTreasure} = this.props;
+    axios
+    .post('/api/treasure/user', {treasureURL})
+    .then(res => {
+      addMyTreasure(res.data);
+      this.setState({
+        treasureURL: ''
+      })
+    })
+    .catch(err => {
+      alert(err.response.request.response)
+    })
   }
 
   render() {
